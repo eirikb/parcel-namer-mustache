80f0b113fe8599e991d26f90b141cea7f244459b
Eirik Brandtzæg <eirikb@eirikb.no>
2021-04-03 13:37:12 +0200

    Update logic
    Return null when not found.
    Remove the nullthrowsthingy.
    This ensures it should pass the naming on to the next namer.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 // @ts-ignore
 import { Namer } from '@parcel/plugin';
 import path from 'path';
-import nullthrows from 'nullthrows';
 
 export default new Namer({
-  name({ bundle, bundleGraph }: any) {
-    const bundleGroup = bundleGraph.getBundleGroupsContainingBundle(bundle)[0];
-    const entryFilePath = nullthrows(
-      bundle
-        .getEntryAssets()
-        .find((a: any) => a.id === bundleGroup.entryAssetId)
-    ).filePath;
+  name({ bundle, bundleGraph }: any): string | null {
+    const bundleGroup = (bundleGraph.getBundleGroupsContainingBundle(bundle) ??
+      [])[0];
+    if (bundleGroup === undefined) return null;
+
+    const entryFilePath = bundle
+      .getEntryAssets()
+      .find((a: any) => a.id === bundleGroup.entryAssetId)?.filePath;
+    if (entryFilePath === undefined) return null;
+
     return path.basename(entryFilePath).replace(/\.mustache/i, '');
   },
 });
